2a9c552ed01a523fd757a16fcae57a30bad5f4b6
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-06-20 18:30:45 +0100

    Missing ;

--- commands/basicInformation.js
+++ commands/basicInformation.js
@@ -2,7 +2,7 @@ const blockchainMap = require("./utils/blockchainMap");
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 	const endOfFirstCommand = message.content.indexOf(" ")-1;
-	const coinName = message.content.substr(1,endOfFirstCommand > 0 ? endOfFirstCommand : undefined)
+	const coinName = message.content.substr(1,endOfFirstCommand > 0 ? endOfFirstCommand : undefined);
 	client.execute(`../server/farmr_server.exe ${message.author.id} --blockchain ${blockchainMap[coinName]}`, message, true);
 };
 
