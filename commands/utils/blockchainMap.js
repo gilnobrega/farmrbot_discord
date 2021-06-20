cc2e202e26f1af37bf299acfc409e521f4500702
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-06-21 00:22:23 +0100

    Added upper case variant of each command dirty fix

--- commands/utils/blockchainMap.js
+++ commands/utils/blockchainMap.js
@@ -1,8 +1,14 @@
 module.exports = blockchainMap = {
     "farmr": "xch",
+    "Farmr": "xch",
     "full": "xch",
+    "Full" : "xch",
+    "Workers" : "xch",
     "workers": "xch",
     "chia": "xch",
+    "Chia": "xch",
     "flax": "xfx",
-    "chaingreen": "cgn"
+    "Flax": "xfx",
+    "chaingreen": "cgn",
+    "Chaingreen": "cgn"
 }
